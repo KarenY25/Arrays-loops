6687429d877739cd41768514727fb2cb8f2ae46c
KarenY25 <karenypineda25@gmail.com>
2024-04-26 18:56:32 -0600

    Tareaversionfinalfinal

--- Arrays.js
+++ Arrays.js
@@ -133,7 +133,7 @@ function positiveDom(arr) {
 
 }
 
-/**Given an array, return a shorter array following these steps:
+/** 7.Given an array, return a shorter array following these steps:
 - Split the array into two equal parts*. If unequal, remove the middle element to obtain two equal arrays.
 - Sum each number of the first part with the inverse numbers of the second part.
 - Divide each number of the resulting array by 2. */
